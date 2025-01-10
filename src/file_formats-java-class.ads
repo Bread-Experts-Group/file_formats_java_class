45948529f303ec1aaab115554d831561c69bb2ac
ATPStorages <tcgytsc@gmail.com>
2025-01-10 12:50:48 -0600

    Testing B

--- src/file_formats-java-class.ads
+++ src/file_formats-java-class.ads
@@ -331,8 +331,8 @@ package File_Formats.Java.Class is
       Major_Version : u2.Big_Endian;
       Constant_Pool : Constant_Pool_Vectors.Vector;
       Access_Flags  : Class_File_Access_Flags;
-      This_Class    : Class_Constant_Pool_Entry_Access;
-      Super_Class   : Class_Constant_Pool_Entry_Access_Optional;
+      --  This_Class    : Class_Constant_Pool_Entry_Access;
+      --  Super_Class   : Class_Constant_Pool_Entry_Access_Optional;
       Interfaces    : Interface_Vectors.Vector;
       Fields        : Field_Vectors.Vector;
       Methods       : Method_Vectors.Vector;
