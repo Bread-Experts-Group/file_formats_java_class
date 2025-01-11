404da2479ebc49e32f192455182527d596dc8ccc
ATPStorages <tcgytsc@gmail.com>
2025-01-11 08:08:57 -0600

    Simple attribute reading (add the file)

---
+++ src/file_formats-java-class-read_attribute_vector.adb
@@ -0,0 +1,41 @@
+pragma Restrictions (No_Exception_Propagation);
+
+-----------------------
+-- Read_Field_Vector --
+-----------------------
+
+separate (File_Formats.Java.Class)
+procedure Read_Attribute_Vector
+  (Stream : not null access Ada.Streams.Root_Stream_Type'Class;
+   Item   : out Attribute_Vectors.Vector;
+   Pool   : Constant_Pool_Maps.Map)
+is
+   Name           : Utf_8_Constant_Pool_Entry;
+   Attribute_Type : Class_File_Attribute_Type;
+begin
+   for Index in 1 .. u2.Big_Endian'Input (Stream) loop
+      Name :=
+        Utf_8_Constant_Pool_Entry
+          (Pool.Element (i2.Big_Endian'Input (Stream)));
+      begin
+         Attribute_Type :=
+           Class_File_Attribute_Type'Value (Name.Utf_Bytes.all);
+      exception
+         when others =>
+            Attribute_Type := Other;
+      end;
+      case Attribute_Type is
+         when others =>
+            declare
+               Data : Raw_Data (1 .. u4.Big_Endian'Input (Stream));
+            begin
+               Raw_Data'Read (Stream, Data);
+               Item.Append
+                 (Class_File_Attribute'
+                    (Attribute_Type => Attribute_Type,
+                     Name_Ref       => Name,
+                     Data           => new Raw_Data'(Data)));
+            end;
+      end case;
+   end loop;
+end Read_Attribute_Vector;
