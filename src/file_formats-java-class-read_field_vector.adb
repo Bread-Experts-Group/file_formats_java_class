bed82aa027a16fd2c4b6e629444be6371e3e5a9b
ATPStorages <tcgytsc@gmail.com>
2025-01-11 07:49:21 -0600

    Field attribute read

--- src/file_formats-java-class-read_field_vector.adb
+++ src/file_formats-java-class-read_field_vector.adb
@@ -21,10 +21,17 @@ begin
       declare
          Name       : constant Constant_Pool_Entry := Pool.Element (Constant_Pool_Index'Input (Stream));
          Descriptor : constant Constant_Pool_Entry := Pool.Element (Constant_Pool_Index'Input (Stream));
+         Field      : Class_File_Field (Environment);
       begin
          case Environment is
-            when CLASS => Item.Append (Class_File_Field'(Environment => CLASS, Access_Flags_Class => Access_Flags, Name_Ref => Utf_8_Constant_Pool_Entry (Name), Descriptor_Ref => Utf_8_Constant_Pool_Entry (Descriptor), others => <>));
-            when IS_INTERFACE => Item.Append (Class_File_Field'(Environment => IS_INTERFACE, Access_Flags_Others => Class_File_Field_Access_Flags_Any (Access_Flags), Name_Ref => Utf_8_Constant_Pool_Entry (Name), Descriptor_Ref => Utf_8_Constant_Pool_Entry (Descriptor), others => <>));
+            when CLASS =>
+               Field := (Environment => CLASS, Access_Flags_Class => Access_Flags, Name_Ref => Utf_8_Constant_Pool_Entry (Name), Descriptor_Ref => Utf_8_Constant_Pool_Entry (Descriptor), others => <>);
+               Read_Attribute_Vector (Stream, Field.Attributes);
+               Item.Append (Field);
+            when IS_INTERFACE =>
+               Field := (Environment => IS_INTERFACE, Access_Flags_Others => Class_File_Field_Access_Flags_Any (Access_Flags), Name_Ref => Utf_8_Constant_Pool_Entry (Name), Descriptor_Ref => Utf_8_Constant_Pool_Entry (Descriptor), others => <>);
+               Read_Attribute_Vector (Stream, Field.Attributes);
+               Item.Append (Field);
          end case;
       end;
       Ada.Text_IO.Put_Line (Item'Image);
