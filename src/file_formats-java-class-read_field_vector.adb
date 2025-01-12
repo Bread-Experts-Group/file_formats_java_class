0c00d99f0a904637210ef3207f9d82474ec982f6
ATPStorages <tcgytsc@gmail.com>
2025-01-11 20:19:22 -0600

    MA seek 1

--- src/file_formats-java-class-read_field_vector.adb
+++ src/file_formats-java-class-read_field_vector.adb
@@ -1,3 +1,4 @@
+with Ada.Text_IO;
 with Ada.Unchecked_Conversion;
 
 -----------------------
@@ -11,15 +12,20 @@ procedure Read_Field_Vector
    Pool        : Constant_Pool_Maps.Map;
    Environment : Class_File_Environment)
 is
+   Exp : u2.Big_Endian;
    Access_Flags : Class_File_Field_Access_Flags;
    function u2_To_Field_Access_Flags is new
      Ada.Unchecked_Conversion (u2.Big_Endian, Class_File_Field_Access_Flags);
 begin
    for Index in 1 .. u2.Big_Endian'Input (Stream) loop
       Access_Flags := u2_To_Field_Access_Flags (u2.Big_Endian'Input (Stream));
+      u2.Big_Endian'Read (Stream, Exp);
+      if Exp not in 1 .. u2.Big_Endian (i2.Big_Endian'Last) then
+         Ada.Text_IO.Put_Line (Exp'Image);
+      end if;
       declare
          Name       : constant Constant_Pool_Entry :=
-           Pool.Element (Constant_Pool_Index'Input (Stream));
+           Pool.Element (Constant_Pool_Index (Exp));
          Descriptor : constant Constant_Pool_Entry :=
            Pool.Element (Constant_Pool_Index'Input (Stream));
       begin
