e38d917a96b7cb34ae1e2f157205a524de21cbfa
ATPStorages <tcgytsc@gmail.com>
2025-01-13 08:17:14 -0600

    Exp 41

--- src/file_formats-java-class-read_constant_pool_map.adb
+++ src/file_formats-java-class-read_constant_pool_map.adb
@@ -51,10 +51,8 @@ is
 
    procedure Handle_Incomplete_Entry (Index : Constant_Pool_Index) is
    begin
-      --  Ada.Text_IO.Put_Line (Index'Image);
       if not Incomplete_Map.Contains (Index) then
          return;
-         --  raise Constraint_Error with "Possible misalignment. " & "No incomplete entry for index" & Index'Image & " incomplete map: (" & Incomplete_Map.Length'Image & " /" & Incomplete_Map.Last_Key'Image & " )";
       end if;
 
       declare
